4b6308cfc70cb34ffb1a23356227af8b34c8d6ed
Hrexandro <marcin_nowak_hrx@o2.pl>
2023-05-07 21:16:59 +0200

    add prepend, size, head, and tail methods

--- linkedList.js
+++ linkedList.js
@@ -1,9 +1,9 @@
 //Build the following functions in your linked list class:
 // append(value) adds a new node containing value to the end of the list - DONE
-// prepend(value) adds a new node containing value to the start of the list
-// size returns the total number of nodes in the list
-// head returns the first node in the list
-// tail returns the last node in the list
+// prepend(value) adds a new node containing value to the start of the list  - DONE
+// size returns the total number of nodes in the list - DONE
+// head returns the first node in the list - DONE
+// tail returns the last node in the list - DONE
 // at(index) returns the node at the given index
 // pop removes the last element from the list
 // contains(value) returns true if the passed in value is in the list and otherwise returns false.
@@ -25,6 +25,27 @@ function linkedListFactory() {
         this.append(value, node.nextNode);
       }
     },
+    prepend: function (value) {
+      let copiedHead = this.nextNode
+      this.nextNode = nodeFactory(value, copiedHead)
+    },
+    size: function (currentNode = this) {
+      if (currentNode.nextNode === null) {
+        return 0
+      } else {
+        return 1 + this.size(currentNode.nextNode)
+      }
+    },
+    head: function() {
+      return this.nextNode
+    },
+    tail: function(node = this) {
+      if (node.nextNode === null) {
+        return node
+      } else {
+        return this.tail(node.nextNode)
+      }
+    }
   };
 }
 
@@ -36,7 +57,9 @@ function nodeFactory(value = null, nextNode = null) {
 }
 
 let exampleLinkedList = linkedListFactory();
-exampleLinkedList.append();
-exampleLinkedList.append("bob");
-console.log(exampleLinkedList)
-console.log(exampleLinkedList.nextNode);
+exampleLinkedList.append("first");
+exampleLinkedList.prepend("the new first");
+exampleLinkedList.append("3");
+exampleLinkedList.append("4");
+let tail = exampleLinkedList.tail()
+console.log(tail)
